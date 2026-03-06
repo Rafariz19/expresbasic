0659487f1e254c5c97331592bc9b712e854aaaa1
Rafariz19 <yukizuna99@gmail.com>
2026-03-06 14:17:06 +0700

    Update

--- routes/kategori.js
+++ routes/kategori.js
@@ -1,21 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const connection = require("../config/database.js")
 
-var connection = require('../config/database.js');
-
-router.get('/', function(req, res, next) {
-    connection.query('select * from kategori order by id_kategori desc', function(err, rows) {
-        if (err) {
-            req.flash('error', err);
-        } else {
-            res.render('kategori/index', {
-              title: 'Data Kategori',
-                data: rows
-            });
-        }
-    });
+router.get('/', function(req, res, next){
+  connection.query('select * from kategori order by id_kategori desc', function(err, rows){
+  if (err) {
+    req.flash('error',err)
+  }else{
+    res.render('kategori/index',{
+    title: 'kategori dta',
+        data: rows
+      });
+  }
+  });
 });
-
 
 // GET create kategori
 router.get('/create', function(req, res, next) {
@@ -25,13 +23,41 @@ router.get('/create', function(req, res, next) {
   })
 });
 
+// Edit Kategori
+router.get('/edit/(:id)', function(req, res, next) {
+  let id = req.params.id;
+  connection.query('select * from kategori where id_kategori =' + id, function(err, rows){
+    if (err) {
+      req.flash('error', 'Gagal memmuat data')
+    }else{
+      res.render('kategori/edit',{
+        id: rows[0].id_kategori,
+        nama_kategori: rows[0].nama_kategori
+      })
+    }
+  })
+})
+
+// Delete kategori
+router.get('/delete/(:id)', function(req, res){
+  let id = req.params.id;
+  connection.query('delete from kategori where id_kategori =' + id,function(err){
+  if (err) {
+    req.flash('error', 'Gagal menghapus data');
+  }else{
+    req.flash('success','Data terhapus!');
+  }
+  res.redirect('/kategori');
+})
+});
+
 // POST store kategori
 router.post('/store', function (req, res, next) {
   try {
     let { nama_kategori } = req.body;
     let Data = {
       nama_kategori
-    };
+    };  
 
     connection.query(
       'INSERT INTO kategori SET ?', Data, function (err, result) {
@@ -48,5 +74,32 @@ router.post('/store', function (req, res, next) {
     res.redirect('/kategori');
   }
 });
+
+
+// POST update kategori
+router.post('/update/:id', (req, res) => {
+  try {
+    let id = req.params.id;
+    let {nama_kategori}= req.body;
+    let Data = {
+      nama_kategori
+    };  
+
+    connection.query(
+      'update kategori set? where id_kategori ='+ id, Data, function (err, result) {
+        if (err) {
+          req.flash('error', 'Gagal memperbarui data!');
+        } else {
+          req.flash('success', 'Berhasil memperbarui data!');
+        }
+        res.redirect('/kategori');
+      }
+    );
+  } catch (error) {
+    req.flash('error', 'Terjadi kesalahan pada fungsi');
+    res.redirect('/kategori');
+  }
+});
+
 
 module.exports = router;
