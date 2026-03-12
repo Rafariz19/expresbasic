c73a6f545e96e06ff3de4b9c4fb29b110d2f9782
Rafariz19 <yukizuna99@gmail.com>
2026-03-12 10:23:10 +0700

    Finishing kategori

--- routes/kategori.js
+++ routes/kategori.js
@@ -1,46 +1,40 @@
 var express = require('express');
 var router = express.Router();
-const connection = require("../config/database.js")
+const connection = require("../config/database.js");
+const model_kategori = require('../Model/model_kategori.js');
 
-router.get('/', function(req, res, next){
-  connection.query('select * from kategori order by id_kategori desc', function(err, rows){
-  if (err) {
-    req.flash('error',err)
-  }else{
-    res.render('kategori/index',{
-    title: 'kategori dta',
+router.get('/', async function(req, res, next) {
+    let rows = await model_kategori.getAll();
+
+    res.render('kategori/index', {
+        title: 'Data Kategori',
         data: rows
-      });
-  }
+    });
   });
-});
-
-// GET create kategori
-router.get('/create', function(req, res, next) {
-  res.render('kategori/create', {
-    title: 'Tambah Kategori',
-    nama_kategori: ''
-  })
+  
+  // GET create kategori
+  router.get('/create', function(req, res, next) {
+    res.render('kategori/create', {
+      title: 'Tambah Kategori',
+      nama_kategori: ''
+    })
+    
 });
 
 // Edit Kategori
-router.get('/edit/(:id)', function(req, res, next) {
+router.get('/edit/(:id)', async function(req, res, next) {
   let id = req.params.id;
-  connection.query('select * from kategori where id_kategori =' + id, function(err, rows){
-    if (err) {
-      req.flash('error', 'Gagal memmuat data')
-    }else{
-      res.render('kategori/edit',{
+  let rows = await  model_kategori.getid(id);
+        res.render('kategori/edit',{
         id: rows[0].id_kategori,
         nama_kategori: rows[0].nama_kategori
       })
-    }
   })
-})
 
 // Delete kategori
-router.get('/delete/(:id)', function(req, res){
+router.get('/delete/(:id)', async function(req, res){
   let id = req.params.id;
+  await model_kategori.delete(id);
   connection.query('delete from kategori where id_kategori =' + id,function(err){
   if (err) {
     req.flash('error', 'Gagal menghapus data');
@@ -52,23 +46,15 @@ router.get('/delete/(:id)', function(req, res){
 });
 
 // POST store kategori
-router.post('/store', function (req, res, next) {
+router.post('/store', async function (req, res, next) {
   try {
     let { nama_kategori } = req.body;
     let Data = {
       nama_kategori
     };  
 
-    connection.query(
-      'INSERT INTO kategori SET ?', Data, function (err, result) {
-        if (err) {
-          req.flash('error', 'Gagal menyimpan data!');
-        } else {
-          req.flash('success', 'Berhasil menyimpan data!');
-        }
-        res.redirect('/kategori');
-      }
-    );
+    await model_kategori.Store(Data);
+    res.redirect('/kategori');
   } catch (error) {
     req.flash('error', 'Terjadi kesalahan pada fungsi');
     res.redirect('/kategori');
@@ -77,24 +63,17 @@ router.post('/store', function (req, res, next) {
 
 
 // POST update kategori
-router.post('/update/:id', (req, res) => {
+router.post('/update/:id', async function (req, res, next) {
   try {
     let id = req.params.id;
-    let {nama_kategori}= req.body;
+    let {nama_kategori} = req.body;
     let Data = {
       nama_kategori
     };  
 
-    connection.query(
-      'update kategori set? where id_kategori ='+ id, Data, function (err, result) {
-        if (err) {
-          req.flash('error', 'Gagal memperbarui data!');
-        } else {
-          req.flash('success', 'Berhasil memperbarui data!');
-        }
-        res.redirect('/kategori');
-      }
-    );
+    await model_kategori.update(id, Data);
+    req.flash('succes', 'Berhasil mengupdate');
+    res.redirect('/kategori');
   } catch (error) {
     req.flash('error', 'Terjadi kesalahan pada fungsi');
     res.redirect('/kategori');
